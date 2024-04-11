2b34679b5ca9a0649767033685179044898a3318
ivan.gluhodedov <ivan.gluhodedov>
2024-04-11 17:30:15 +0300

    added create default store if not founc

--- app.go
+++ app.go
@@ -7,33 +7,52 @@ import (
 	"task/task_model"
 )
 
+func CheckDefaultDirExists(path string) error {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		err = nil
+		err = os.Mkdir(fmt.Sprintf("%v/%v", os.Getenv("HOME"), ".taski"), 0775)
+		if err != nil {
+			return err
+		}
+
+		_, err = os.Create(fmt.Sprintf("%v/%v", os.Getenv("HOME"), ".taski/task.do.json"))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func main() {
 	var taskList = new(task_model.TaskList)
 	var lang = "RUS"
+	var defaultPath = fmt.Sprintf("%v/%v", os.Getenv("HOME"), ".taski/task.do.json")
 
 	if langEnv := os.Getenv("TASKI_LANG"); langEnv != "" {
 		if langEnv != "ENG" && langEnv != "RUS" {
 			fmt.Printf("only Russian (RUS) and English (ENG) language are available")
 			os.Exit(1)
 		}
-
 		lang = langEnv
 	}
 
-	if os.Getenv("TASKI_STORE") == "" {
-		taskList = task_model.NewTaskList(
-			fmt.Sprintf("%v/%v", os.Getenv("HOME"), ".taski/task.do.json"),
-			lang,
-		)
-	} else {
-		taskList = task_model.NewTaskList(
-			os.Getenv("TASKI_STORE"),
-			lang,
-		)
+	if path := os.Getenv("TASKI_STORE"); path != "" {
+		defaultPath = path
 	}
 
+	if err := CheckDefaultDirExists(defaultPath); err != nil {
+		fmt.Printf("Error check store directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	taskList = task_model.NewTaskList(
+		defaultPath,
+		lang,
+	)
+
 	if err := taskList.LoadFromStore(); err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error load file from store: %v \n", err)
 		os.Exit(1)
 	}
 
@@ -45,7 +64,7 @@ func main() {
 
 	err := rootCmd.Execute()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error execute command: %v\n", err)
 		os.Exit(1)
 	}
 }
